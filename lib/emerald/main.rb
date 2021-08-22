00e19e91936c42170e2a74aa555a61502ec6c332
Martin Nyaga <martin@martinnyaga.com>
2021-08-22 01:37:48 +0100

    Addbasic options to bin/emerald

--- lib/emerald/main.rb
+++ lib/emerald/main.rb
@@ -1,11 +1,36 @@
+require "optparse"
+
 module Emerald
   class Main
-    def self.run
-      if ARGV.count > 0
-        code = File.read(ARGV[0])
-        Emerald::Interpreter.new.interprete(code)
-      else
-        Emerald::Repl.run
+    class << self
+      def run
+        options = parse_options
+        if options.count == 0
+          Emerald::Repl.run
+        elsif options[:execute]
+          Emerald::Interpreter.new.interprete(options[:execute])
+        else
+          code = File.read(ARGV[0])
+          Emerald::Interpreter.new.interprete(code)
+        end
+      end
+
+      def parse_options
+        options = {}
+        OptionParser.new do |opts|
+          opts.banner = "Usage: emerald [options]"
+
+          opts.on("-e \"<code>\"", "--execute \"<code>\"", "Execute one line of source code") do |code|
+            options[:execute] = code.gsub("\\n", "\n")
+          end
+
+          opts.on("-h", "--help", "Show usage") do |code|
+            puts opts
+            exit
+          end
+        end.parse!
+
+        options
       end
     end
   end
