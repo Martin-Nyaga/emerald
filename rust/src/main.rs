bc5584006d5e96d5a22fae053cac1e4d643d1f1b
Martin Nyaga <martin@martinnyaga.com>
2021-09-27 17:41:58 +0100

    Add function to generate sample bytecode

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,30 @@
 mod vm;
 use vm::{Chunk, VM};
 
-fn main() {
-    let path = std::env::args().nth(1).expect("filename must be provided");
+fn example_bytecode() -> Vec<u8> {
+    let mut code = vec![];
 
-    println!("reading file: {}", path);
-    let bytes = std::fs::read(path).map_err(|e| format!("{}", e)).unwrap();
+    // Constants
+    code.push(0);
+    code.push(1);
+
+    code.push(1);
+    code.extend((120 as u64).to_be_bytes());
+
+    // Operations
+    code.push(1);
+    code.push(0);
+
+    code.push(0);
+    code
+}
+
+fn main() {
+    // let path = std::env::args().nth(1).expect("filename must be provided");
+
+    // println!("reading file: {}", path);
+    // let bytes = std::fs::read(path).map_err(|e| format!("{}", e)).unwrap();
+    let bytes = example_bytecode();
 
     println!("interpreting bytecode");
     let vm = VM::new();
